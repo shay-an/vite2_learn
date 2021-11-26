da1f8f46a5d55d7e6df33cca94e2de68bc36c264
shay_an <axy7779@outlook.com>
2021-11-26 18:05:48 +0800

    布局框架

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import ElementPlus from 'element-plus'
 import router from './router/index'
 import store from './store/index'
+import 'element-plus/dist/index.css'
 
 createApp(App)
     .use(router)
+    .use(ElementPlus)
     .use(store)
     .mount('#app')
---
+++ src/views/layout.vue
@@ -0,0 +1,32 @@
+<template>
+  <el-container>
+    <el-aside>
+      <Aside></Aside>
+    </el-aside>
+    <el-main>
+      <el-header>
+        <Header></Header>
+      </el-header>
+      <el-main>
+        <router-view></router-view>
+      </el-main>
+      <el-footer></el-footer>
+    </el-main>
+  </el-container>
+</template>
+
+<script lang="ts">
+import Aside from './aside.vue'
+import Header from './header.vue'
+import { defineComponent } from 'vue'
+export default defineComponent({
+  components: {
+    Aside,
+    Header
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+
+</style>
